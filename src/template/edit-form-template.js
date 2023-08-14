3d42e76159e5dd8c8a071d28639bf63ed888565d
Dimaggio <zodiak-d@yandex.ru>
2023-08-14 12:00:13 +0300

    final mock (data.js)

--- src/template/edit-form-template.js
+++ src/template/edit-form-template.js
@@ -1,4 +1,4 @@
-const getTripEditFormItemTemplate = () => (
+const getTripEditFormItemTemplate = (data) => (
   `<li class="trip-events__item">
     <form class="event event--edit" action="#" method="post">
       <header class="event__header">
--- src/view/edit-form-view.js
+++ src/view/edit-form-view.js
@@ -2,8 +2,12 @@ import {createElement} from '../render.js';
 import {getTripEditFormItemTemplate} from '../template/edit-form-template.js';
 
 export default class TripEditFormItemView {
+  constructor({data}) {
+    this.data = data;
+  }
+
   getTemplate() {
-    return getTripEditFormItemTemplate();
+    return getTripEditFormItemTemplate(this.data);
   }
 
   getElement() {
