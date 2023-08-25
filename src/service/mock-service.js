380e8d725b420ef29a49300715869959acde3e2a
Dimaggio <zodiak-d@yandex.ru>
2023-08-25 23:36:38 +0300

    mock-service.js WIP

--- src/service/mock-service.js
+++ src/service/mock-service.js
@@ -1,12 +1,15 @@
+import { generatePoint } from '../mock/data.js';
+import { POINT_COUNT, DESTINATIONS, OFFERS } from '../const.js';
+
 export default class MockService {
   #destinations = [];
-  #offers = [];
   #points = [];
+  #offers = [];
 
   constructor() {
-    this.#destinations = '';
-    this.#offers = '';
-    this.#points = '';
+    this.#destinations = DESTINATIONS;
+    this.#offers = OFFERS;
+    this.#points = this.#generatePoints();
   }
 
   getDestinations() {
@@ -14,16 +17,14 @@ export default class MockService {
   }
 
   getOffers() {
-    return this.#destinations;
+    return this.#offers;
   }
 
   getPoints() {
     return this.#points;
   }
 
-  #generateDestinations() {}
-
-  #generateOffers() {}
-
-  #generatePoints() {}
+  #generatePoints() {
+    return Array.from({ length: POINT_COUNT }, () => generatePoint());
+  }
 }
